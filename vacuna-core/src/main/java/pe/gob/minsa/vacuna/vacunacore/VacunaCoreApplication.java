7cc5b1b202bd0e1bb8bee5168c0c046de606a304
yesmi.torres <yesmi.torres@qualifacts.com>
2020-11-07 00:14:49 -0500

    main class

---
+++ vacuna-core/src/main/java/pe/gob/minsa/vacuna/vacunacore/VacunaCoreApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.minsa.vacuna.vacunacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacunaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacunaCoreApplication.class, args);
+	}
+
+}
